304cbaa3c8a5ff7c397d0bf47f2067afad053cc2
corprodoc <corprodoc@gmail.com>
2025-07-24 09:14:51 +0530

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,5 +1,27 @@
-import main from './function.js'
+window.addEventListener("message", async function(event) {
+  const { origin, data: { key, params } } = event;
 
-export default async function handler(args) {
-  return await main(args)
-}
+  let result;
+  let error;
+  try {
+    result = await window.function(...params);
+  } catch (e) {
+    result = undefined;
+    try {
+      error = e.toString();
+    } catch (e) {
+      error = "Exception can't be stringified.";
+    }
+  }
+
+  const response = { key };
+  if (result !== undefined) {
+    // FIXME: Remove `type` once that's in staging
+    response.result = { value: result };
+  }
+  if (error !== undefined) {
+    response.error = error;
+  }
+
+  event.source.postMessage(response, "*");
+});
