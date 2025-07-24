6d03bbb11d91aa59adb8762f55bb181d057b417d
corprodoc <corprodoc@gmail.com>
2025-07-24 09:36:35 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,25 +1,34 @@
-import { PDFDocument, rgb, StandardFonts } from 'pdf-lib'
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
 import fetch from 'node-fetch'
 import { upload } from '@glideapps/plugin-uploader'
 
 export default async function ({ pdfUrl, headerText }) {
-  const existingPdfBytes = await fetch(pdfUrl).then(r => r.arrayBuffer())
+  // 1. Download the existing PDF
+  const res = await fetch(pdfUrl)
+  const existingPdfBytes = await res.arrayBuffer()
+
+  // 2. Load the PDF
   const pdfDoc = await PDFDocument.load(existingPdfBytes)
-  const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
   const pages = pdfDoc.getPages()
 
-  for (const page of pages) {
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  // 3. Add header to each page
+  pages.forEach((page) => {
     const { width, height } = page.getSize()
     page.drawText(headerText, {
       x: 50,
       y: height - 50,
-      size: 14,
+      size: 12,
       font,
       color: rgb(0, 0, 0),
     })
-  }
+  })
 
-  const modifiedBytes = await pdfDoc.save()
-  const uploadedUrl = await upload(modifiedBytes, 'headered.pdf')
-  return { outputPdfUrl: uploadedUrl }
+  // 4. Save modified PDF
+  const modifiedPdfBytes = await pdfDoc.save()
+
+  // 5. Upload to Glide & return URL
+  const uploadedUrl = await upload(modifiedPdfBytes, 'modified.pdf')
+  return uploadedUrl
 }
